0de138076d852703beab5f5b96921b9bfa310c1a
JaveriyaNooriSys <javeriya.noorisys@gmail.com>
2023-03-01 13:47:50 +0530

    first commit

---
+++ app/Http/Controllers/PlanController.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Booking;
+use App\Models\Plan;
+use App\Models\Subscriptions;
+use Carbon\Carbon;
+use Stripe\Stripe;
+
+class PlanController extends Controller
+{
+    public function __construct() {
+        
+        $this->middleware(function ($request, $next) {
+            $this->stripe = new \Stripe\StripeClient(
+                env('STRIPE_SECRET')
+            );
+            $this->stripe_key = Stripe::setApiKey(env('STRIPE_SECRET'));
+            $this->user = auth()->user();
+            return $next($request);
+        });
+    }
+
+    public function index()
+    {
+        $data['plans'] = Plan::all();
+        return view('dashboard',$data);
+    }
+
+    public function checkout(Request $request)
+    {
+        $plan = Plan::where('plan_id', '=', $request->plan_id)->first();
+        $success_url = url('subscription-success');
+        $cancel_url = url('subscription-fail');
+
+        if (!empty($plan)) {
+            $session = \Stripe\Checkout\Session::Create([
+                'success_url' => $success_url,
+                'cancel_url' => $cancel_url,
+                'payment_method_types' => ['card'],
+                'line_items' => [
+                        ['price' => $plan->plan_id, 'quantity' => 1],
+                    ],
+                'mode' => 'subscription',
+                'currency' => env('STRIPE_CURRENCY'),
+            ]);
+
+            if ($session) {
+                $booking_data = [
+                    'session_id' => $session->id,
+                    'user_id' => $this->user->id,
+                    'subscription_id' => $session->subscription ? $session->subscription : '',
+                    'customer_id' => $session->customer ? $session->customer : '',
+                    'amount_paid' => $session->amount_total/100,
+                    'currency' => $session->currency,
+                    'payment_status' => $session->payment_status,
+                    'session_status' => $session->status,
+                    'created_at' => Carbon::now()
+                ];
+                $insert = Booking::insert($booking_data);
+                if ($insert) {
+                    return redirect()->to($session->url);
+                } else {
+                    return redirect()->back()->with('fail','Something went wrong, Please try again...');
+                }
+            } else {
+                return redirect()->back()->with('fail','Something went wrong, Please try again...');
+            }
+        } else {
+            return redirect()->back()->with('fail','Invalid Plan, Please try again...');
+        }
+    }
+
+    public function payment(Request $request)
+    {
+        $amount = $request->amount;
+        $success_url = url('subscription-success');
+        $cancel_url = url('subscription-fail');
+        if ($amount) {
+            $line_items = [
+                [
+                  'price_data'=> [
+                    'currency'=> env('STRIPE_CURRENCY'),
+                    'unit_amount'=> $amount * 100,
+                    'product_data'=> [
+                        'name'=> "One Time Payment",
+                        ],
+                    ],
+                  'quantity'=> 1,
+                ],
+            ];
+            $session = \Stripe\Checkout\Session::Create([
+                'success_url' => $success_url,
+                'cancel_url' => $cancel_url,
+                'payment_method_types' => ['card'],
+                'line_items' => $line_items,
+                'mode' => 'payment',
+                'customer_creation' => 'always',
+            ]);
+
+            if ($session) {
+                $booking_data = [
+                    'session_id' => $session->id,
+                    'user_id' => $this->user->id,
+                    'subscription_id' => $session->payment_intent ? $session->payment_intent : '',
+                    'customer_id' => $session->customer ? $session->customer : '',
+                    'amount_paid' => $session->amount_total/100,
+                    'currency' => $session->currency,
+                    'payment_status' => $session->payment_status,
+                    'session_status' => $session->status,
+                    'created_at' => Carbon::now()
+                ];
+                $insert = Booking::insert($booking_data);
+                if ($insert) {
+                    return redirect()->to($session->url);
+                } else {
+                    return redirect()->back()->with('fail','Something went wrong, Please try again...');
+                }
+            } else {
+                return redirect()->back()->with('fail','Something went wrong, Please try again...');
+            }
+        } else {
+            return redirect()->back()->with('fail','Something went wrong, Please try again...');
+        }
+        
+    }
+
+    public function subscriptionSuccess()
+    {
+        return redirect()->to('plans')->with('success','Payment Successfull!');
+    }
+
+    public function subscriptionFail()
+    {
+        return redirect()->to('plans')->with('fail','Unable to Pay, Please try again...');
+    }
+
+    public function webhook(Request $request)
+    {
+        $endpoint_secret = env('STRIPE_WEBHOOK_SECRET');
+
+        $payload = $request->getContent();
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sig_header, $endpoint_secret
+            );
+        } catch(\UnexpectedValueException $e) {
+            // Invalid payload
+            http_response_code(400);
+            exit();
+        } catch(\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            http_response_code(400);
+            exit();
+        }
+
+        // Handle the event
+        switch ($event->type) {
+            case 'checkout.session.completed':
+                $session = $event->data->object;
+                $booking_data = [
+                    'subscription_id' => $session->subscription ? $session->subscription : $session->payment_intent,
+                    'customer_id' => $session->customer ? $session->customer : '',
+                    'payment_status' => $session->payment_status,
+                    'session_status' => $session->status,
+                    'updated_at' => Carbon::now()
+                ];
+                Booking::where('session_id',$session->id)->update($booking_data);
+                break;
+            case 'checkout.session.expired':
+                $session = $event->data->object;
+                $booking_data = [
+                    'session_status' => $session->status,
+                    'updated_at' => Carbon::now()
+                ];
+                Booking::where('session_id',$session->id)->update($booking_data);
+                break;
+            case 'customer.created':
+                $customer = $event->data->object;
+            case 'customer.updated':
+                $customer = $event->data->object;
+            case 'customer.subscription.created':
+                $subscription = $event->data->object;
+                $booking = Booking::where('subscription_id',$subscription->id)->first();
+                $item = $subscription->items->data[0];
+                $subscription_data = [
+                    'booking_id'    => $booking->id,
+                    'user_id' => $booking->user_id,
+                    'customer_id' => $subscription->customer ? $subscription->customer : '',
+                    'subscription_id' => $subscription->id ? $subscription->id : '',
+                    'plan_id' => $item->plan->id,
+                    'currency' => $subscription->currency,
+                    'plan_interval' => $item->plan->interval,
+                    'plan_period_start' => date('Y-m-d', $subscription->current_period_start),
+                    'plan_period_end' => date('Y-m-d', $subscription->current_period_end),
+                    'amount_paid' => $item->plan->amount/100,
+                    'payment_status' => $booking->payment_status,
+                    'subscription_status' => $subscription->status,
+                    'created_at' => Carbon::now()
+                ];
+                Subscriptions::insert($subscription_data);
+                break;
+            case 'customer.subscription.deleted':
+                $subscription = $event->data->object;
+            case 'customer.subscription.paused':
+                $subscription = $event->data->object;
+            case 'customer.subscription.resumed':
+                $subscription = $event->data->object;
+            case 'customer.subscription.updated':
+                $subscription = $event->data->object;
+                $item = $subscription->items->data[0];
+                $subscription_data = [
+                    'plan_period_start' => date('Y-m-d', $subscription->current_period_start),
+                    'plan_period_end' => date('Y-m-d', $subscription->current_period_end),
+                    'amount_paid' => $item->plan->amount/100,
+                    'subscription_status' => $subscription->status,
+                    'updated_at' => Carbon::now()
+                ];
+                Subscriptions::where('subscription_id', '=', $subscription->id)->update($subscription_data);
+                break;
+            case 'invoice.created':
+                $invoice = $event->data->object;
+            case 'invoice.deleted':
+                $invoice = $event->data->object;
+            case 'invoice.finalization_failed':
+                $invoice = $event->data->object;
+            case 'invoice.finalized':
+                $invoice = $event->data->object;
+            case 'invoice.paid':
+                $invoice = $event->data->object;
+            case 'invoice.sent':
+                $invoice = $event->data->object;
+            case 'invoice.upcoming':
+                $invoice = $event->data->object;
+            case 'invoice.updated':
+                $invoice = $event->data->object;
+            case 'payment_intent.canceled':
+                $paymentIntent = $event->data->object;
+            case 'payment_intent.created':
+                $paymentIntent = $event->data->object;
+            case 'payment_intent.payment_failed':
+                $paymentIntent = $event->data->object;
+            case 'payment_intent.succeeded':
+                $paymentIntent = $event->data->object;
+                $booking = Booking::where('subscription_id',$paymentIntent->id)->first();
+                $paymentIntent_data = [
+                    'booking_id'    => $booking->id,
+                    'user_id' => $booking->user_id,
+                    'customer_id' => $paymentIntent->customer ? $paymentIntent->customer : '',
+                    'subscription_id' => $paymentIntent->id ? $paymentIntent->id : '',
+                    'currency' => $paymentIntent->currency,
+                    'amount_paid' => $paymentIntent->amount_received/100,
+                    'payment_status' => $booking->payment_status,
+                    'subscription_status' => $paymentIntent->status,
+                    'created_at' => Carbon::now()
+                ];
+                Subscriptions::insert($paymentIntent_data);
+                break;
+            // ... handle other event types
+            default:
+                echo 'Received unknown event type ' . $event->type;
+        }
+
+        http_response_code(200);
+    }
+}
---
+++ resources/views/components/welcome.blade.php
@@ -0,0 +1,84 @@
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+<div class="container" style="padding-top: 50px; padding-bottom: 50px;">
+    <div class="row">
+        @if (Session::has('success'))
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                </symbol>
+                <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                </symbol>
+                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </symbol>
+            </svg>
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+                <div>{{Session::get('success')}}</div>
+            </div>
+        @elseif (Session::has('fail'))
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                </symbol>
+                <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                </symbol>
+                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                </symbol>
+            </svg>
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                <div>{{Session::get('fail')}}</div>
+            </div>
+        @endif
+        @foreach ($plans as $plan)
+        <div class="col-4">
+            <div class="card">
+                <h5 class="card-header text-center">{{ $plan->name }}&nbsp;<span class="text-muted"style="font-size:14px;">({{ $plan->price.$plan->currency }})</span></h5>
+                <div class="card-body">
+                    <ul style="list-style: square; padding-left:40%;">
+                        <li>Feature1</li>
+                        <li>Feature2</li>
+                        <li>Feature3</li>
+                        <li>Feature4</li>
+                        <li>Feature5</li>
+                    </ul>
+                </div>
+                <div class="card-footer text-center">
+                    <form action="{{ route('checkout') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="plan_id" value="{{ $plan->plan_id }}">
+                        <button type="submit" class="btn btn-primary">Subscribe</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        <div class="col-4">
+            <div class="card">
+                <h5 class="card-header text-center">One Time Payment &nbsp;<span class="text-muted"style="font-size:14px;">(999€)</span></h5>
+                <div class="card-body">
+                    <ul style="list-style: square; padding-left:40%;">
+                        <li>Feature1</li>
+                        <li>Feature2</li>
+                        <li>Feature3</li>
+                        <li>Feature4</li>
+                        <li>Feature5</li>
+                    </ul>
+                </div>
+                <div class="card-footer text-center">
+                    <form action="{{ route('payment') }}" method="post">
+                        @csrf
+                        <input type="hidden" name="amount" value="{{ 999 }}">
+                        <button type="submit" class="btn btn-primary">Pay Now</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
